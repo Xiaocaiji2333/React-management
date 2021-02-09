a99f7f3544d4ab3dbf8f22e83054fa1bfe255e9f
ljq <3107258593@qq.com>
2021-02-09 18:07:48 +0800

    validCode

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -14,8 +14,24 @@ import './login.less';
 const Item = Form.Item;
 
 export default class Login extends Component {
+  formRef = React.createRef();
+
+  validDate = (_, value) => {
+    if (value.length <= 4) {
+      return Promise.reject('密码不能低于4位！')
+    } else {
+      return Promise.resolve()
+    }
+  }
+
   login = () => {
-    
+    // console.log(this.formRef.current)
+    // const { username, password } = this.formRef.current.getFieldsValue()
+    this.formRef.current.validateFields(['username', 'password']).then(value => {
+      console.log(value)
+    }).catch(err => {
+      console.log(err)
+    })
   }
 
   render() {
@@ -27,12 +43,17 @@ export default class Login extends Component {
         </header>
         <section className='login-content'>
           <h3>用户登录</h3>
-          <Form onSubmit={ this.login } className='login-form'>
-            <Item>
+          <Form ref={ this.formRef } onSubmitCapture={ this.login } className='login-form'>
+            <Item name='username' rules={ [{ required: true, message: '用户名不能为空！' },
+              { pattern: /^[a-zA-Z0-9_]+$/, message: '用户名必须是字母，数字或下划线组成！' }] }>
               <Input prefix={ <UserOutlined style={ { color: 'rgba(0,0,0,.25)' } }/> }
                 type='text' placeholder='用户名'></Input>
             </Item>
-            <Item>
+            <Item name='password' rules={ [{ required: true, message: '密码不能为空！' },
+              { pattern: /^[a-zA-Z0-9]+$/, message: '密码必须是字母或数字组成！' },
+              { max: 16, message: '密码不能高于16位！' },
+              { validator: this.validDate }
+              ] }>
               <Input prefix={ <LockOutlined style={ { color: 'rgba(0,0,0,.25)' } }/> }
                 type='password' placeholder='密码'></Input>
             </Item>
